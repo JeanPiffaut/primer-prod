a57a7cad059f638ee44178131558a7bcd72ce8ee
Jean Piffaut <40282095+JeanPiffaut@users.noreply.github.com>
2024-04-04 18:20:49 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola profe
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/antonio-gallardo/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Antonio
         </a>
       </header>
     </div>
